61c04a43f84af3e4121eee1dc27faf71d65a5b99
Geco Games Studios <gecogameslimited@gmail.com>
2026-05-15 08:25:54 +0200

    Patch 0.2.0

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # Include auth URLs
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Direct logout URL
     path('__reload__/', include("django_browser_reload.urls")),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
